a548df4305d7af8d2c36392cd6dc858cb5ca8866
kdyzm <kdyzm@foxmail.com>
2020-03-07 16:07:14 +0800

    完成todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import '../assets/css/TodoList.css'
+
 class TodoList extends React.Component {
     constructor(props) {
         super(props);
@@ -33,9 +35,19 @@ class TodoList extends React.Component {
     }
 
     handleTodo=(e)=>{
-        this.setState({
-            todo: e.target.value
-        })
+        console.log(e.keyCode)
+        if(e.keyCode==13){
+            var todo = this.refs.todo.value;
+            var list=this.state.list;
+            list.push({
+                name: todo,
+                checked: false
+            });
+            this.setState({
+                todo: list
+            })
+            this.refs.todo.value = "";
+        }
     }
 
     handleChange=(key)=>{
@@ -55,21 +67,21 @@ class TodoList extends React.Component {
     }
     render() {
         return (
-            <div>
-                <h2>{this.state.title}</h2>
+            <div className="wrapper">
+                <h2 className="title">{this.state.title}</h2>
                 <hr/>
-                <input value={this.state.todo} onChange={this.handleTodo}/>
+                回车完成输入：<input className="todoInput" ref="todo" onKeyUp={this.handleTodo}/>
 
                 <h3>待做事项：</h3>
-                <ul>
+                <ul className="todo">
                     {
                         this.state.list
                         .map((value,key)=>{
                             if(!value.checked){
                                 return (
                                     <li key={key}>
-                                        <input type="checkbox" checked={value.checked} onChange={this.handleChange.bind(this,key)}/>{value.name}--
-                                        <input type="button" value="删除" onClick={this.handleDelete.bind(this,key)}/>
+                                        <input className="checkbox" type="checkbox" checked={value.checked} onChange={this.handleChange.bind(this,key)}/>{value.name}
+                                        <input class="delete" type="button" value="删除" onClick={this.handleDelete.bind(this,key)}/>
                                     </li>
                                 )
                             }
@@ -78,15 +90,15 @@ class TodoList extends React.Component {
                 </ul>
 
                 <h3>已完成事项：</h3>
-                <ul>
+                <ul className="done">
                     {
                         this.state.list
                         .map((value,key)=>{
                             if(value.checked){
                                 return (
                                     <li key={key}>
-                                        <input type="checkbox" checked={value.checked} onChange={this.handleChange.bind(this,key)}/>{value.name}--
-                                        <input type="button" value="删除" onClick={this.handleDelete.bind(this,key)}/>
+                                        <input className="checkbox" type="checkbox" checked={value.checked} onChange={this.handleChange.bind(this,key)}/>{value.name}
+                                        <input class="delete" type="button" value="删除" onClick={this.handleDelete.bind(this,key)}/>
                                     </li>
                                 )
                             }
