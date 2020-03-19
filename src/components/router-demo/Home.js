f687f025f4d58fa16834b6d6bf175869fed46d0a
kdyzm <kdyzm@foxmail.com>
2020-03-19 23:35:17 +0800

    使用antd，遗留bug：导航栏不响应

--- src/components/router-demo/Home.js
+++ src/components/router-demo/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {Button} from 'antd';
 
 class Home extends Component {
     constructor(props) {
@@ -11,6 +12,7 @@ class Home extends Component {
     render() {
         return (
             <div>
+                <Button type="primary">按钮组件</Button>
                 <h2>{this.state.title}</h2>
                 <hr/>
                 <button><Link to="/login">跳转到登录页面</Link></button>
